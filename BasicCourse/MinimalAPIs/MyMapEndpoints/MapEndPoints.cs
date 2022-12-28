100d51780ca756636cb869d4a23474f2003f8bc7
congThuan23 <conthuan701@gmail.com>
2022-12-28 22:56:22 +0700

    feat: add minimalapis

---
+++ BasicCourse/MinimalAPIs/MyMapEndpoints/MapEndPoints.cs
@@ -0,0 +1,36 @@
+﻿
+namespace MinimalAPIs.MyMapEndpoints
+{
+    public static class MapEndPoints
+    {
+        public static WebApplication MapUserEndPoint(this WebApplication app)
+        {
+            app.MapGet(pattern: "/Users", () => new string[] { "admin", "guest" });
+            app.MapGet(pattern: "/Users/{name}", (string name) => $"Hello {name}");
+            return app;
+        }
+
+        public static WebApplication MapWeathereForcast(this WebApplication app)
+        {
+            var summaries = new[]
+            {
+                "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            };
+
+            app.MapGet("/weatherforecast", () =>
+            {
+                var forecast = Enumerable.Range(1, 5).Select(index =>
+                    new WeatherForecast
+                    (
+                        DateTime.Now.AddDays(index),
+                        Random.Shared.Next(-20, 55),
+                        summaries[Random.Shared.Next(summaries.Length)]
+                    ))
+                    .ToArray();
+                return forecast;
+            })
+            .WithName("GetWeatherForecast");
+            return app;
+        }
+    }
+}
